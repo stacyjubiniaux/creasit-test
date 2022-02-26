177ad63e8e3fb409f781d45e688dc88abb7cd288
stacyjubiniaux <stacy.jubiniaux@gmail.com>
2022-02-26 20:05:46 +0100

    Fin du style et ajout des fonctionnalités

--- js/script.js
+++ js/script.js
@@ -7,3 +7,49 @@ var swiper = new Swiper(".mySwiper", {
     delay: 3000,
   },
 });
+
+/* IMPRIMER LA PAGE */
+
+function imprimer_page(){
+  var bouton = document.getElementsByClassName('imprimer');
+  bouton.window.print();
+}
+
+
+/* VOIR PLUS D'ARTICLE & MODIFIER LE NOMBRE DE RÉSULTATS */
+
+const btn = document.querySelector('.bouton-afficher');
+const articles = document.querySelector('.voir-articles');
+
+btn.addEventListener('click', () => {
+  // Afficher plus d'article 
+  articles.classList.toggle('is-visible');
+  var resultats = document.getElementById("resultats");
+  // Modifier la valeur du résultat 
+  if (resultats.innerHTML === "Vous avez 6 résultats sur 12") {
+    resultats.innerHTML = "Vous avez 12 résultats sur 12";
+  } else {
+    resultats.innerHTML = "Vous avez 6 résultats sur 12";
+  }
+  // Modifier le contenu du bouton
+  if (btn.innerHTML === "Afficher plus") {
+    btn.innerHTML = "Afficher moins";
+  } else {
+    btn.innerHTML = "Afficher plus";
+  }
+});
+
+/* OUVERTURE FERMETURE CHECKBOX PLUS DE FILTRES */
+
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
